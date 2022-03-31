cdf370224548875cb2998d35c2a645916a7882fe
wsl <radiowebmasters@gmail.com>
2022-03-31 07:32:57 +0100

    Ubuntu

---
+++ build/CMakeFiles/tddsockets.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tddsockets.dir/src/tests/main.cpp.o"
+  "CMakeFiles/tddsockets.dir/src/xpcommon.cpp.o"
+  "tddsockets"
+  "tddsockets.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tddsockets.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- include/tests/test_badly_behaved_client.h
+++ include/tests/test_badly_behaved_client.h
@@ -115,7 +115,9 @@ inline int test_badly_behaved_client(int ctr = 2) {
                     return 0;
                 });
 
-            assert(rx.return_value == 10057); // at least in doze
+            printf("Return value: %ld: %s\n", rx.return_value, c.last_error_string().c_str());
+            assert(rx.return_value == 10057 || // doze
+                    rx.return_value == 107); // Ubuntu
             std::ignore = rx;
 
         }; // while()
